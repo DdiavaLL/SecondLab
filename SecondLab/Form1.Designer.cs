b3ccff3babc847fc9051921231aa49fe93a00bd5
Tereshchenko Vlad <ddiavall@yandex.ru>
2020-09-21 22:53:05 +0300

    Small changes in main form.

--- SecondLab/Form1.Designer.cs
+++ SecondLab/Form1.Designer.cs
@@ -136,7 +136,7 @@
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Лабораторная №1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
